4214583d121dda1c0a39f6b2ebed2ef11d39c0a1
Daniel Lewis <thecontrarian42@gmail.com>
2012-02-18 22:00:21 -0600

    Uploading some Wibit.net Lab Files

---
+++ Wibit.net/ConsoleFormatting.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is a %s, this is a %d", "string", 7);
+	return 0;
+}
---
+++ Wibit.net/getpreviouslystored.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	int c;
+	printf("%d\n", c);
+	return 0;
+}
