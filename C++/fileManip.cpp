d00457a97ef97766210f76a710e49628ef3c4d89
Daniel Lewis <thecontrarian42@gmail.com>
2012-07-12 21:58:32 -0500

    Created C++ learning directory

---
+++ C++/fileManip.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+	//ofstream outFile;
+	int x = 10, y = 10;
+	cout 	<< x 
+			<< " " 
+			<< y 
+			<< " " 
+			<< (x-3)/(y-5) 
+			<< " I'M SO EXCITED!" 
+			<< endl;
+	return 0;
+}
